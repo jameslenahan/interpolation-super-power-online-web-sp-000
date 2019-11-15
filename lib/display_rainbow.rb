a534d6ee5e2d8c7c7c3bc2ba4c35f7cb5b9250a2
James Lenahan <james.k.lenahan@gmail.com>
2019-11-15 13:19:48 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,8 +3,8 @@ def display_rainbow['red', 'orange', 'yellow', 'green', ' blue', 'indigo', 'viol
   puts "R: #{display_rainbow(0)},"
   puts "O: #{display_rainbow(1)},"
   puts "Y: #{display_rainbow(2)},"
-  puts "G: #{display_rainbow[3]},"
-  puts "B: #{display_rainbow[4]},"
-  puts "I: #{display_rainbow[5]},"
-  puts "V: #{display_rainbow[6]},"
+  puts "G: #{display_rainbow(3)},"
+  puts "B: #{display_rainbow(4)},"
+  puts "I: #{display_rainbow(5)},"
+  puts "V: #{display_rainbow(6)},"
 end
